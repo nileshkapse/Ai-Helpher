0370aa9076a4fe56aac8dd107eac0e5108bb0774
nileshkapse18 <nilesh.kapse@in.musafir.com>
2023-10-19 19:35:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./App.css";
+import LandingPages from "./Pages/LandingPages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ThemeSwitcher from "./Pages/ThemeSwitcher";
+import { useState } from "react";
 
 function App() {
+  
+  const [theme,setTheme]=useState("white-theme")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className={`App ${theme}`}>
+      <BrowserRouter>
+        <ThemeSwitcher setTheme={setTheme} theme={theme} /> {/* Place the ThemeSwitcher component here */}
+        <Routes>
+          <Route path="/" element={<LandingPages />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
