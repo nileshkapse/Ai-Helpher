5d0ab92bbe59f084bc94e6931060eedf514ced89
nileshkapse18 <nilesh.kapse@in.musafir.com>
2023-10-20 15:45:42 +0530

    Added API call

--- src/Pages/LandingPages.js
+++ src/Pages/LandingPages.js
@@ -1,9 +1,10 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Typewriter from "./TypeWriter";
 import ThemeSwitcher from "./ThemeSwitcher";
 
 function LandingPages() {
-  const [res,setRes]=useState([]);
+  const [inputString, setInputString] = useState('');
+  const [res, setRes] = useState([]);
   const paragraphs = [
     " OpenAI is an artificial intelligence research organization and company that focuses on developing advanced AI technologies. It was founded in December 2015 by Elon Musk, Sam Altman, Greg Brockman, Ilya Sutskever, Wojciech Zaremba, and John Schulman.",
     "   OpenAI's mission is to ensure that artificial general intelligence(AGI), which refers to highly autonomous systems that outperformhumans at most economically valuable work, benefits all of humanity.OpenAI conducts research in a variety of AI-related fields, includingnatural language processing, reinforcement learning, computer vision,and more. The organization has been responsible for the development of several prominent AI models, including GPT-3 (Generative Pre-trained Transformer 3) and GPT-2.",
@@ -11,6 +12,21 @@ function LandingPages() {
     // Add more paragraphs as needed
   ];
 
+  useEffect(() => {
+    console.log(res);
+  }, [res]);
+
+  const stringsplit=()=>{
+    const substrings = inputString.split(/\n|<p>/);
+    setRes(substrings);
+  }
+
+  // const addStringToArray = (newString) => {
+
+
+
+  //   setRes([...res, newString]);
+  // };
   const [inputSearch, setInputSearch] = useState("");
   async function sendRequest(event) {
     try {
@@ -24,10 +40,12 @@ function LandingPages() {
           },
         }
       );
-      const data = await response.json(); // Assuming the API returns JSON data
-      setRes(data);
-      const responseContainer = document.getElementById("response-container");
-      responseContainer.textContent = `Here is your Answer: ${data.body.generated_text}`;
+      const data = await response.json();
+      // Assuming the API returns JSON data
+      setInputString(data.body.generated_text);
+      stringsplit()
+      // addStringToArray(data.body.generated_text);
+      console.log(data.body.generated_text);
     } catch (error) {
       console.error("Error sending request:", error);
     }
@@ -47,22 +65,22 @@ function LandingPages() {
     <div className="">
       <div className="logo-container">
         <img src="/aihelper-dark.png" alt="" />
-
       </div>
       <div class="chatgpt-input-container">
         <textarea
           ref={textareaRef}
           className="chatgpt-input"
           placeholder="Ask ChatGPT..."
-          onInput={handleInput}
-          onChange={(e)=>setInputSearch(e.target.value)}
+          onInput={(e) => setInputSearch(e.target.value)}
+          onChange={(e) => setInputSearch(e.target.value)}
         ></textarea>
-        <button class="chatgpt-submit-button"
-        onClick={()=>sendRequest()}>Submit</button>
+        <button class="chatgpt-submit-button" onClick={() => sendRequest()}>
+          Submit
+        </button>
       </div>
 
       <div className="result-div">
-        <Typewriter paragraphs={paragraphs} />
+        <Typewriter paragraphs={res} />
 
         <div className="textbox-container">
           <div className="text-field-heading">
